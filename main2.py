7b50df741eef6f85a3a4108e2f085ec61855c6f4
Raj kumar <drajkumar01234@gmail.com>
2023-05-24 11:24:31 +0530

    adding main2.py

--- main2.py
+++ main2.py
@@ -1,1 +1,8 @@
-print("hello i am main2.py ")
+import win32com.client
+
+speakers  = win32com.client.Dispatch("SAPI.SpVoice")
+
+while 1:
+    print("Enter the work")
+    s = input()
+    speakers.Speak(s)
